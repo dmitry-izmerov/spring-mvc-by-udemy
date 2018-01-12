6c8c2e6c35c0875f5f8e50b6961afa4ab64c0bde
demi <idd90i@gmail.com>
2018-01-12 22:24:56 +0300

    Converted product to entity

--- src/main/java/ru/demi/springmvc/models/Product.java
+++ src/main/java/ru/demi/springmvc/models/Product.java
@@ -5,13 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String description;
 	private BigDecimal price;
