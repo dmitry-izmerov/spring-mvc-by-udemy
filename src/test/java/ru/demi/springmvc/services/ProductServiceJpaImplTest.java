5de583245c4fc563dcfcc592b63970bf0f95407d
demi <idd90i@gmail.com>
2018-01-14 14:22:46 +0300

    added integration test for product service jpa implementation

---
+++ src/test/java/ru/demi/springmvc/services/ProductServiceJpaImplTest.java
@@ -0,0 +1,30 @@
+package ru.demi.springmvc.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.demi.springmvc.config.JpaIntegrationConfig;
+import ru.demi.springmvc.models.Product;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JpaIntegrationConfig.class)
+@ActiveProfiles({"jpa"})
+public class ProductServiceJpaImplTest {
+
+	@Autowired
+	private ProductService productService;
+
+	@Test
+	public void testGetAllProducts() {
+		List<Product> allProducts = productService.getAllProducts();
+
+		assertEquals(3, allProducts.size());
+	}
+}
