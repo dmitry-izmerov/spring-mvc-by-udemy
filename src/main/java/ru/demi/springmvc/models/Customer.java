0fba012029f0d99778a5385b6d0131555edbbba1
demi <idd90i@gmail.com>
2018-01-15 22:28:09 +0300

    created customer entity

---
+++ src/main/java/ru/demi/springmvc/models/Customer.java
@@ -0,0 +1,33 @@
+package ru.demi.springmvc.models;
+
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Version;
+
+@Data
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@Version
+	private Integer version;
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private User user;
+}
