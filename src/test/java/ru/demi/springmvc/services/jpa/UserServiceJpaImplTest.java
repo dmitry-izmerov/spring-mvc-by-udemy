d7e72dd0e8c9ff14d22f7ca6432d2f0a58feff52
demi <idd90i@gmail.com>
2018-01-16 23:01:30 +0300

    added test

--- src/test/java/ru/demi/springmvc/services/jpa/UserServiceJpaImplTest.java
+++ src/test/java/ru/demi/springmvc/services/jpa/UserServiceJpaImplTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.demi.springmvc.config.JpaIntegrationConfig;
+import ru.demi.springmvc.models.Customer;
 import ru.demi.springmvc.models.User;
 import ru.demi.springmvc.services.UserService;
 
@@ -30,5 +31,23 @@ public class UserServiceJpaImplTest {
 
 		assertNotNull(saved.getId());
 		assertNotNull(saved.getEncryptedPassword());
+	}
+
+	@Test
+	public void shouldSaveUserWithCustomer() {
+		User user = new User();
+		user.setUsername("user name");
+		user.setPassword("pass");
+		Customer customer = new Customer();
+		customer.setFirstName("Test");
+		customer.setLastName("Testov");
+		user.setCustomer(customer);
+
+		User saved = userService.save(user);
+
+		assertNotNull(saved.getId());
+		assertNotNull(saved.getEncryptedPassword());
+		assertNotNull(saved.getCustomer());
+		assertNotNull(saved.getCustomer().getId());
 	}
 }
